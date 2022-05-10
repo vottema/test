a8b35b417c3fafa344e84d2eca093cf9092a981d
Artem <breakingbad171@mail.ru>
2022-05-10 11:35:58 +0300

    fix

--- client/src/components/Tasks/Tasks.jsx
+++ client/src/components/Tasks/Tasks.jsx
@@ -20,7 +20,7 @@ const Tasks = ({ tasks }) => {
   const change = (event) => {
     event.preventDefault()
     const id = event.target.id
-    console.log(event.target.name);
+    console.log(event.target.changeText);
     const text = inputChangeText.current.value
     const updateText = { id, text }
     dispatch(fetchUpdateTasks(updateText))
