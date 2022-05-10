613f44a4cd9322c8b6b6892fc39e904c7fe79361
Artem <breakingbad171@mail.ru>
2022-05-10 03:07:13 +0300

    make it better

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { rootReducer } from './reducers/rootReducer'
 import thunk from 'redux-thunk'
-const composeEnhancers = composeWithDevTools({});
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
