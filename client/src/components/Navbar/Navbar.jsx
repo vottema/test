9b4f0b725734f47f7074a8bd0a82469a16bfbeb0
Artem <breakingbad171@mail.ru>
2022-05-10 08:19:15 +0300

    fix

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -19,15 +19,8 @@ const Navbar = () => {
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Test</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            {/* <span className="navbar-toggler-icon"></span> */}
-            <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle nav-item dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>          </button>
+            <span className="navbar-toggler-icon"></span>
+              </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
