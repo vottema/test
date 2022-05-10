11bee3bffc1cdb9830ca0b08fba59890e027e00a
Artem <breakingbad171@mail.ru>
2022-05-10 03:23:46 +0300

    fix

--- config/config.js
+++ config/config.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const sessionConfig = require('./sessionConfig')
 const config = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
